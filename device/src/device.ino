bade2647af45e33758885a48d491aac059dd5d7a
Jeroen F.J. Laros <jlaros@fixedpoint.nl>
2018-07-29 22:08:32 +0200

    Rudimentary serial interface.

--- device/src/device.ino
+++ device/src/device.ino
@@ -1,29 +1,60 @@
 /* MAXIM 7301 port I/O expander. */
 #include "max7301.h"
 
+#define CMD_ENABLE 0x00
+#define CMD_DISABLE 0x01
+#define CMD_PINMODE 0x02
+#define CMD_GET_PINMODE 0x03
+#define CMD_CONF_TD 0x04
+#define CMD_ENABLE_TD 0x05
+#define CMD_DIG_READ 0x06
+#define CMD_DIG_WRITE 0x07
+#define CMD_READ 0x08
+#define CMD_WRITE 0x09
+
 MAX7301 max7301(2, 3, 4, 5, false);
 
 
 void setup(void) {
-  max7301.enable();
-  max7301.pinMode(12, GPIO_INPUT_PULLUP);
-  max7301.pinMode(22, GPIO_OUTPUT);
-
-  Serial.begin(0x9600);
+  Serial.begin(9600);
 }
 
 void loop(void) {
-  static byte led = LOW;
+  char cmd[3];
 
   if (Serial.available()) {
-    Serial.println(max7301.digitalRead(12));
+    Serial.readBytes(cmd, 3);
+    switch (cmd[0]) {
+      case CMD_ENABLE:
+        max7301.enable();
+        break;
+      case CMD_DISABLE:
+        max7301.disable();
+        break;
+      case CMD_PINMODE:
+        max7301.pinMode((byte)cmd[1], (byte)cmd[2]);
+        break;
+      case CMD_GET_PINMODE:
+        Serial.write(max7301.getPinMode((byte)cmd[1]));
+        break;
+      case CMD_CONF_TD:
+        max7301.configureTransitionDetection((byte)cmd[1], (bool)cmd[2]);
+        break;
+      case CMD_ENABLE_TD:
+        max7301.enableTransitionDetection();
+        break;
+      case CMD_DIG_READ:
+        Serial.write(max7301.digitalRead((byte)cmd[1]));
+        break;
+      case CMD_DIG_WRITE:
+        max7301.digitalWrite((byte)cmd[1], (byte)cmd[2]);
+        break;
+      case CMD_READ:
+        Serial.write(max7301.read((byte)cmd[1]));
+        break;
+      case CMD_WRITE:
+        max7301.write((byte)cmd[1], (byte)cmd[2]);
+        break;
+    }
   }
-  if (!max7301.digitalRead(12)) {
-    max7301.digitalWrite(22, led);
-    led = !led;
-  }
-  else {
-    max7301.digitalWrite(22, LOW);
-  }
-  delay(500);
 }
--- host/host.py
+++ host/host.py
@@ -1,9 +1,66 @@
 #!/usr/bin/env python
 import serial
+import struct
+import time
+
+
+class MAX7301(object):
+    def __init__(self):
+        self._connection = serial.Serial('/dev/ttyACM0')
+        self._commands = [
+            'enable', 'disable', 'pinmode', 'get_pinmode', 'conf_td',
+            'enable_td', 'dig_read', 'dig_write', 'read', 'write']
+        self._modes = ['', 'output', 'input', 'input_pullup']
+        self._values = ['low', 'high']
+
+    def cmd(self, command, *args):
+        self._connection.write(struct.pack(
+            'BBB', self._commands.index(command),
+            *(args + (0,) * (2 - len(args)))))
+
+    def pin_mode(self, pin, mode):
+        self.cmd('pinmode', pin, self._modes.index(mode))
+
+    def digital_read(self, pin):
+        self.cmd('dig_read', pin)
+
+        return ord(self._connection.read(1))
+
+    def digital_write(self, pin, data):
+        self.cmd('dig_write', pin, self._values.index(data))
+
+    def read(self, address):
+        self.cmd('read', address)
+
+        return ord(self._connection.read(1))
 
 
 def main():
-    connection = serial.Serial('/dev/ttyACM0')
+    max7301 = MAX7301()
+
+    max7301.cmd('enable')
+    max7301.pin_mode(12, 'input_pullup')
+    max7301.pin_mode(22, 'output')
+
+    max7301.pin_mode(31, 'output')
+    max7301.pin_mode(24, 'input_pullup')
+    max7301.cmd('conf_td', 24, True)
+    max7301.cmd('enable_td')
+
+    for address in range(0x60):
+        data = max7301.read(address)
+        print '0x{:02x}: 0x{:02x} 0b{:08b}'.format(address, data, data)
+
+    while True:
+        if not max7301.digital_read(12):
+            max7301.digital_write(22, 'high')
+            time.sleep(0.5)
+            max7301.digital_write(22, 'low')
+        else:
+            max7301.digital_write(22, 'low')
+        time.sleep(0.5)
+
+        max7301.cmd('enable_td')
 
 
 if __name__ == '__main__':
